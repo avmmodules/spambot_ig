930196a8daa577a322e4ca179e893aad742e1d76
luisalejandrovm <LuisAlejandroVM@hotmail.com>
2021-03-18 23:05:05 -0600

    first commit

---
+++ spambot_ig.py
@@ -0,0 +1,21 @@
+'''
+    Description:
+    Send messages from python to instagram.
+
+    Author: AlejandroV
+    Video: https://youtu.be/P23eUMc6vEE
+'''
+import pyautogui
+from time import sleep
+
+sleep(5)
+
+# open a simple file
+with open('test.txt','r') as file:
+    # iterate
+    for line in file:
+        # write the lines on page opened
+        pyautogui.typewrite(line)
+
+        # press enter (send message)
+        pyautogui.press("enter")
